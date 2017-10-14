320cf51e65f4bcc6b609437fced0becb7790983e
bahriddin <bahriddin.abdiev@gmail.com>
2017-10-14 20:03:54 +1100

    Indentation resolved.

--- teamStrategyA.py
+++ teamStrategyA.py
@@ -32,205 +32,196 @@ ans.append('I_AM_SCARED_GHOST_ENEMY_FAR')
 ans.append('I_AM_ACTIVE_GHOST_ENEMY_FAR')
 ans.append('I_AM_PACMAN_ENEMY_FAR')
 
+
 class myCustomAgent(CaptureAgent):
+    def registerInitialState(self, gameState):
+        self.start = gameState.getAgentPosition(self.index)
+        print "StartState: " + str(self.start)
+        CaptureAgent.registerInitialState(self, gameState)
 
-  def registerInitialState(self, gameState):
-    self.start = gameState.getAgentPosition(self.index)
-    print "StartState: " + str(self.start)
-    CaptureAgent.registerInitialState(self, gameState)
+    def chooseAction(self, gameState):
+        # start = time.time() #for time evaluation
 
+        actions = gameState.getLegalActions(self.index)
+        values = [self.evaluate(gameState, a) for a in actions]
+        maxValue = max(values)
+        bestActions = [a for a, v in zip(actions, values) if v == maxValue]
+        a = self.getFoodCount(gameState)
+        foodLeft = len(self.getFood(gameState).asList())
+        if foodLeft <= 2:
+            bestDist = 9999
+            for action in actions:
+                successor = self.getSuccessor(gameState, action)
+                pos2 = successor.getAgentPosition(self.index)
+                dist = self.getMazeDistance(self.start, pos2)
+                if dist < bestDist:
+                    bestAction = action
+                    bestDist = dist
+            return bestAction
 
-  def chooseAction(self, gameState):
-    #start = time.time() #for time evaluation
+        agent = ""
+        if (self.index == 1):
+            agent = "Dark Blue"
+        else:
+            agent = "Light Blue"  # index = 3
 
-    actions = gameState.getLegalActions(self.index)
-    values = [self.evaluate(gameState, a) for a in actions]
-    maxValue = max(values)
-    bestActions = [a for a, v in zip(actions, values) if v == maxValue]
-    a = self.getFoodCount(gameState)
-    foodLeft = len(self.getFood(gameState).asList())
-    if foodLeft <= 2:
-      bestDist = 9999
-      for action in actions:
+        print "chosen Action of Agent " + agent + ": " + random.choice(bestActions)
+        return random.choice(bestActions)
+
+    def getSuccessor(self, gameState, action):
+        successor = gameState.generateSuccessor(self.index, action)
+        pos = successor.getAgentState(self.index).getPosition()
+        if pos != nearestPoint(pos):
+            return successor.generateSuccessor(self.index, action)
+        else:
+            return successor
+
+    def evaluate(self, gameState, action):
+        features = self.getFeatures(gameState, action)
+        weights = self.getWeights(gameState, action)
+        return features * weights
+
+    def getFeatures(self, gameState, action):
+        features = util.Counter()
         successor = self.getSuccessor(gameState, action)
-        pos2 = successor.getAgentPosition(self.index)
-        dist = self.getMazeDistance(self.start, pos2)
-        if dist < bestDist:
-          bestAction = action
-          bestDist = dist
-      return bestAction
+        features['successorScore'] = self.getScore(successor)
+        return features
 
-    agent = ""
-    if(self.index == 1):
-      agent = "Dark Blue"
-    else:
-      agent = "Light Blue" #index = 3
+    def getWeights(self, gameState, action):
+        return {'successorScore': 1.0}
 
+    def getLocation(self, gameState, action):
+        successor = self.getSuccessor(gameState, action)
+        myState = successor.getAgentState(self.index)
+        xPos, yPos = myState.getPosition()
+        if (xPos <= self.start[0] / 2):
+            return True
+        else:
+            return False
 
+    # get the Enemy Position
+    def getEnemyLocations(self, gameState):
+        enemyLocation = []
+        for enemyPlayers in self.getOpponents(gameState):
+            location = gameState.getAgentPosition(enemyPlayers)
+            if location != None:
+                enemyLocation.append((enemyPlayers, location))
+        return enemyLocation
 
+    # Capsules that are the closest
+    def getClosestCapsules(self, gameState):
+        capsules = self.getCapsules(gameState)
+        min = None
+        closestCap = None
+        if len(capsules) > 0:  # array has locations
+            min = 9999  # random high dist
+            myLoc = gameState.getAgentPosition(self.index)
+            for coords in capsules:
+                dist = self.getMazeDistance(coords, myLoc)
+                if dist < min:
+                    min = dist
+                    closestCap = coords
+        return closestCap
 
-    print "chosen Action of Agent " + agent + ": " + random.choice(bestActions)
-    return random.choice(bestActions)
+    # Enemies that are the closest
+    def getClosestEnemies(self, gameState):
+        location = self.getEnemyLocations(gameState)
+        min = None
+        if len(location) > 0:  # array has locations
+            min = 9999  # random high dist
+            myLoc = gameState.getAgentPosition(self.index)
+            for enemy, coords in location:
+                dist = self.getMazeDistance(coords, myLoc)
+                if dist < min:
+                    min = dist
+        return min
 
-  def getSuccessor(self, gameState, action):
-    successor = gameState.generateSuccessor(self.index, action)
-    pos = successor.getAgentState(self.index).getPosition()
-    if pos != nearestPoint(pos):
-      return successor.generateSuccessor(self.index, action)
-    else:
-      return successor
+    def PacmanInEnemyLoc(self, currentPlayer):
+        return currentPlayer.isPacman
 
+        # ********************** for A Star ********************
 
-  def evaluate(self, gameState, action):
-    features = self.getFeatures(gameState, action)
-    weights = self.getWeights(gameState, action)
-    return features * weights
+    def nullHeuristic(state, problem=None):
+        return 0
 
+    def aStarSearch(problem, heuristic=nullHeuristic):
+        """Question 4"""
+        Queue_Astar = util.PriorityQueue()
+        currentStateXY = problem.getStartState()
+        currentStateAction = []
+        currentStateCost = 0
+        Queue_Astar.push([currentStateXY, currentStateAction, currentStateCost], heuristic(currentStateXY, problem))
+        visited = []
 
-  def getFeatures(self, gameState, action):
-    features = util.Counter()
-    successor = self.getSuccessor(gameState, action)
-    features['successorScore'] = self.getScore(successor)
-    return features
+        while not Queue_Astar.isEmpty():
+            currentState = Queue_Astar.pop()
+            currentStateXY = currentState[0]
+            currentStateAction = currentState[1]
+            currentStateCost = currentState[2]
+            if problem.isGoalState(currentStateXY):
+                return currentStateAction
 
-  def getWeights(self, gameState, action):
-    return {'successorScore': 1.0}
+            if currentStateXY not in visited:
+                visited.append(currentStateXY)
+                successors = problem.getSuccessors(currentStateXY)
+                for children in successors:
+                    currentStateXY = children[0]
+                    currentDir = children[1]
+                    currentChildCost = children[2]
 
-  def getLocation(self,gameState, action):
-    successor = self.getSuccessor(gameState, action)
-    myState = successor.getAgentState(self.index)
-    xPos,yPos= myState.getPosition()
-    if(xPos <= self.start[0]/2):
-        return True
-    else:
-        return False
+                    allActions = currentStateAction + [currentDir]
+                    gN = problem.getCostOfActions(allActions)
+                    hN = heuristic(currentStateXY, problem)
+                    AStarTotalCost = gN + hN
+                    Queue_Astar.push((currentStateXY, allActions, gN), AStarTotalCost)
+        return []
 
+        # ********************** for A Star END ********************
 
-  #get the Enemy Position
-  def getEnemyLocations(self, gameState):
-    enemyLocation = []
-    for enemyPlayers in self.getOpponents(gameState):
-      location = gameState.getAgentPosition(enemyPlayers)
-      if location != None:
-        enemyLocation.append((enemyPlayers, location))
-    return enemyLocation
+    def getClosestFood(self, foodList, position):
+        closestFood = -1
+        FoodPos = -1
+        for food in foodList:
+            currFoodDist = self.getMazeDistance(food, position)
+            if closestFood is 0 or currFoodDist < closestFood:
+                FoodPos = food
+                closestFood = currFoodDist
+        return (FoodPos, closestFood)
 
+    def getFoodCount(self, gameState):
+        foodCount = len(self.getFood(gameState).asList())
+        return foodCount
 
-  #Capsules that are the closest
-  def getClosestCapsules(self, gameState):
-    capsules = self.getCapsules(gameState)
-    min = None
-    closestCap = None
-    if len(capsules) > 0:  # array has locations
-      min = 9999  # random high dist
-      myLoc = gameState.getAgentPosition(self.index)
-      for coords in capsules:
-        dist = self.getMazeDistance(coords, myLoc)
-        if dist < min:
-          min = dist
-          closestCap = coords
-    return closestCap
+    def whoAmI(self, gameState):
+        """
+        It returns global variable values and finds in which situation agent is
+        :param gameState:
+        :return:
+        """
+        myState = gameState.getAgentState(self.index)
+        enemies = self.getOpponents(gameState)
+        isEnemyScared = False
 
-  #Enemies that are the closest
-  def getClosestEnemies(self, gameState):
-    location = self.getEnemyLocations(gameState)
-    min = None
-    if len(location) > 0: #array has locations
-      min = 9999 #random high dist
-      myLoc = gameState.getAgentPosition(self.index)
-      for enemy, coords in location:
-        dist = self.getMazeDistance(coords, myLoc)
-        if dist < min:
-          min = dist
-    return min
+        if len(self.getEnemyLocations(gameState)) > 0:
+            if not myState.isPacman:
+                if myState.scaredTimer > 0:
+                    return I_AM_SCARED_GHOST_ENEMY_CLOSE
+                else:
+                    return I_AM_ACTIVE_GHOST_ENEMY_CLOSE
+            else:
+                for enemyIndex in enemies:
+                    if enemyIndex is not None and gameState.getAgentState(enemyIndex).scaredTimer > 0:
+                        isEnemyScared = True
+                        break
+                if isEnemyScared:
+                    return I_AM_POWERED_PACMAN_ENEMY_CLOSE
+                else:
+                    return I_AM_SIMPLE_PACMAN_ENEMY_CLOSE
+        else:
+            if not myState.isPacman:
+                if myState.scaredTimer > 0:
+                    return I_AM_SCARED_GHOST_ENEMY_FAR
+                else:
+                    return I_AM_ACTIVE_GHOST_ENEMY_FAR
 
-
-  def PacmanInEnemyLoc(self, currentPlayer):
-    return currentPlayer.isPacman
-
-    # ********************** for A Star ********************
-  def nullHeuristic(state, problem=None):
-    return 0
-
-  def aStarSearch(problem, heuristic=nullHeuristic):
-    """Question 4"""
-    Queue_Astar = util.PriorityQueue()
-    currentStateXY = problem.getStartState()
-    currentStateAction = []
-    currentStateCost = 0
-    Queue_Astar.push( [currentStateXY, currentStateAction, currentStateCost], heuristic(currentStateXY, problem))
-    visited = []
-
-    while not Queue_Astar.isEmpty():
-      currentState = Queue_Astar.pop()
-      currentStateXY = currentState[0]
-      currentStateAction = currentState[1]
-      currentStateCost = currentState[2]
-      if problem.isGoalState(currentStateXY):
-        return currentStateAction
-
-      if currentStateXY not in visited:
-        visited.append(currentStateXY)
-        successors = problem.getSuccessors(currentStateXY)
-        for children in successors:
-          currentStateXY = children[0]
-          currentDir = children[1]
-          currentChildCost = children[2]
-
-          allActions = currentStateAction + [currentDir]
-          gN = problem.getCostOfActions(allActions)
-          hN = heuristic(currentStateXY, problem)
-          AStarTotalCost = gN + hN
-          Queue_Astar.push((currentStateXY, allActions, gN), AStarTotalCost)
-    return []
-
-    # ********************** for A Star END ********************
-
-  def getClosestFood(self, foodList, position):
-    closestFood = -1
-    FoodPos = -1
-    for food in foodList:
-      currFoodDist = self.getMazeDistance(food, position)
-      if closestFood  is 0 or currFoodDist < closestFood:
-        FoodPos = food
-        closestFood = currFoodDist
-    return (FoodPos, closestFood)
-
-  def getFoodCount(self,gameState):
-    foodCount = len(self.getFood(gameState).asList())
-    return foodCount
-
-
-  def whoAmI(self, gameState):
-      """
-    It returns global variable values and finds in which situation agent is
-    :param gameState:
-    :return:
-    """
-      myState = gameState.getAgentState(self.index)
-      enemies = self.getOpponents(gameState)
-      isEnemyScared = False
-
-      if len(self.getEnemyLocations(gameState)) > 0:
-          if not myState.isPacman:
-              if myState.scaredTimer > 0:
-                  return I_AM_SCARED_GHOST_ENEMY_CLOSE
-              else:
-                  return I_AM_ACTIVE_GHOST_ENEMY_CLOSE
-          else:
-              for enemyIndex in enemies:
-                  if enemyIndex is not None and gameState.getAgentState(enemyIndex).scaredTimer > 0:
-                      isEnemyScared = True
-                      break
-              if isEnemyScared:
-                  return I_AM_POWERED_PACMAN_ENEMY_CLOSE
-              else:
-                  return I_AM_SIMPLE_PACMAN_ENEMY_CLOSE
-      else:
-          if not myState.isPacman:
-              if myState.scaredTimer > 0:
-                  return I_AM_SCARED_GHOST_ENEMY_FAR
-              else:
-                  return I_AM_ACTIVE_GHOST_ENEMY_FAR
-
-      return I_AM_PACMAN_ENEMY_FAR
+        return I_AM_PACMAN_ENEMY_FAR

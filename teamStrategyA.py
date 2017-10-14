fcfbddf90aff95f8de3dc1b02794638ffd12abeb
Kriti <kritibhardwaj@icloud.com>
2017-10-14 20:22:37 +1100

    added getFoodDiff

--- teamStrategyA.py
+++ teamStrategyA.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -179,6 +179,12 @@ class myCustomAgent(CaptureAgent):
         closestFood = currFoodDist
     return (FoodPos, closestFood)
 
-  def getFoodCount(self,gameState):
-    foodCount = len(self.getFood(gameState).asList())
-    return foodCount
+
+  def getFoodDiff(self,gameState):
+    toEatFoodCount = len(self.getFood(gameState).asList())
+    toDefendFoodCount = len(self.getFoodYouAreDefending(gameState).asList())
+    foodDiff = toEatFoodCount - toDefendFoodCount
+    return foodDiff
+
+
+
